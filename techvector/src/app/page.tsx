b5f4d94878317c19afbb3acc012f043e1c5157e3
Stefan A Tudose <stefan.tudose5@gmail.com>
2025-02-01 23:22:48 +0200

    stylized hero section

--- techvector/src/app/page.tsx
+++ techvector/src/app/page.tsx
@@ -2,8 +2,16 @@ import Image from "next/image";
 import Link from "next/link";
 import ArticleList from "@/ui/articleList";
 import Pagination from "@/ui/pagination";
+import { Geostar } from "next/font/google";
+import { FaArrowCircleDown } from "react-icons/fa";
 
 const host = "localhost:8000"
+
+const geostar = Geostar({
+  weight: "400",
+  subsets: ["latin"],
+});
+
 
 export default async function Home(props: any) {
   const searchParams = await props.searchParams;
@@ -17,20 +25,24 @@ export default async function Home(props: any) {
           </main>
   const totalPages = await response.json();
 
+  const scrollToList = (ref : any) =>{
+    if (ref && ref.current){
+      ref.current.scrollIntoView({ behavior: 'smooth', block: 'start' })
+    }
+  }
+
 
   return (
     <main>
-      <header>
-        <Image src="/TV_logo.png" alt="TechVector" width={200} height={200} />
-        <Link href = "">Conversation List</Link>
-      </header>
-      <div>
-        <h1>TechVector</h1>
-        <h2>An easier way of staying up-to-date with the tech world.</h2>
-        <p>based on all TechCrunch article posted in 2025, powered by OpenAI's gpt-4o-mini, stored in PGVector</p>
-
-        <Link href = "/search_articles">Seach Document</Link>
-        <Link href="/chat">Ask a General Question</Link>
+      <div className="h-[88vh] flex flex-col items-center justify-center space-y-4 pb-25">
+        <h1 className={`text-9xl font-bold ${geostar.className}`}>TechVector</h1>
+        <h2 className="text-4xl text-emerald-500">An easier way of staying up-to-date with the tech world,</h2>
+        <p className="text-center text-xl text-gray-200 max-w-2xl">based on all TechCrunch articles posted in 2025, powered by OpenAI's gpt-4o-mini, stored in PGVector</p>
+        <div className="flex space-x-4 mt-4">
+          <Link className="px-4 py-2 bg-teal-800 text-white rounded-lg" href = "/search_articles">Seach Document</Link>
+          <Link className="px-4 py-2 bg-teal-800 text-white rounded-lg" href="/chat">Ask a General Question</Link>
+        </div>
+        
       </div>
 
       <ArticleList pageNumber={currentPage}/>
@@ -39,4 +51,4 @@ export default async function Home(props: any) {
 
     </main>
   );
-}
+}//<FaArrowCircleDown className="absolute bottom-30 size-15" href={scrollToList}/>
