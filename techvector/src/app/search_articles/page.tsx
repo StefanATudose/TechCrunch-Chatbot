4d563f864cd1ce0d4744f3c729007ec2bb5c3d26
Stefan A Tudose <stefan.tudose5@gmail.com>
2025-02-04 23:26:19 +0200

    finished alpha version of website

--- techvector/src/app/search_articles/page.tsx
+++ techvector/src/app/search_articles/page.tsx
@@ -12,11 +12,13 @@ export default function SearchArticles(){
     const [query, setQuery] = useState<string>();
     const [articles, setArticles] = useState<Article[]> ();
     const [loading, setLoading] = useState<boolean>(false);
+    const [fetchError, setFetchError] = useState<boolean>(false);
     const host = "localhost:8000";
 
     function interstitial(){
         setLoading(true);
         setArticles([]);
+        setFetchError(false);
         search_articles();
     }
 
@@ -28,17 +30,22 @@ export default function SearchArticles(){
 
         const encoded_query = encodeURI(query);
         
-        var response = await fetch(`http://${host}/get_articles_by_query/${encoded_query}`);
-        if (response.status !== 200) {
-            console.error('Failed to fetch articles');
+        try{
+          var response = await fetch(`http://${host}/get_articles_by_query/${encoded_query}`);
+          const data = await response.json();
+          if (data)
+              setArticles(data);     
+          setLoading(false);
+          console.log(data);  
+        }  
+        catch {
+            console.log("res status faultyu");
+            setFetchError(true);
+            setLoading(false);
             return;
         }
 
-        const data = await response.json();
-        if (data)
-            setArticles(data);     
-        setLoading(false);
-        console.log(data);   
+         
     }
 
     return(
@@ -46,7 +53,7 @@ export default function SearchArticles(){
             <h2 className = "text-6xl font-semibold">Search for an article</h2>
             <form action={interstitial} className="bg-gray-900 p-4 rounded-2xl shadow-lg flex items-center space-x-4 w-[60%] my-20">
                 <textarea
-                  placeholder="Ex: Give me an article about Amazon posted in January"
+                  placeholder="Ex: Give me an article about Amazon posted on 10 January"
                   className="flex-1 overflow-y-auto resize-none overflow-x-hidden bg-gray-800 text-white p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-500"
                   value={query}
                   onChange={(event)=>{setQuery(event.target.value);}}
@@ -75,6 +82,14 @@ export default function SearchArticles(){
             {articles?.map((article : any, idx: any) => (
                 <ArticleComponent key = {idx} article = {article} />
             ))}
+            {fetchError && 
+            <div className="bg-red-600 text-black flex flex-col space-y-2 p-4 rounded-2xl shadow-lg text-center">
+              <h1 className="text-xl font-bold">Model Error</h1>
+              <p className="text-md">The query was too complex and couldn't be processed at this time. It might work later.</p>
+              <p className="text-md">This is because for this demo, I used the cheapest model from OpenAI, gpt-4o-mini (due to budget constraints).</p>
+              <p className="text-md">Using gpt-4o would solve this error, but that costs 15 times more, and nobody got money for that.</p>
+            </div>
+            }
         </div>
     )
 }
